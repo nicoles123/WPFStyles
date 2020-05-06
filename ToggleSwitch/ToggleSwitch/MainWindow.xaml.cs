24e070d580d38befef306262fbdaaf1b6f50e3bf
nicoles123 <wsfzjx200809@gmail.com>
2020-05-06 13:22:44 +0800

    add new

---
+++ ToggleSwitch/ToggleSwitch/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace ToggleSwitch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            anim();
+        }
+
+        public async void anim()
+        {
+            CreateAnEllipse(240, 240, 0.4);
+            await Task.Delay(300);
+            CreateAnEllipse(240, 240, 0.4);
+            await Task.Delay(300);
+            CreateAnEllipse(240, 240, 0.4);
+        }
+
+
+        public void CreateAnEllipse(double x, double y, double z)
+        {
+            // Create an Ellipse    
+            Ellipse blueRectangle = new Ellipse();
+            
+            blueRectangle.Height = x;
+            blueRectangle.Width = y;
+            // Create a blue and a black Brush    
+            SolidColorBrush blueBrush = new SolidColorBrush();
+            blueBrush.Color = Color.FromRgb(111,80,220);
+            blueBrush.Opacity = z;
+            SolidColorBrush blackBrush = new SolidColorBrush();
+            blackBrush.Color = Colors.Transparent;
+            // Set Ellipse's width and color    
+            blueRectangle.StrokeThickness = 0;
+            blueRectangle.Stroke = blackBrush;
+            // Fill rectangle with blue color    
+            blueRectangle.Fill = blueBrush;
+            // Add Ellipse to the Grid.    
+            LayoutRoot.Children.Add(blueRectangle);
+
+            DoubleAnimation myDoubleAnimation = new DoubleAnimation();
+            myDoubleAnimation.From = 220;
+            myDoubleAnimation.To = 330;
+            myDoubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(1.5));
+            myDoubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
+            DoubleAnimation myDoubleAnimation2 = new DoubleAnimation();
+            myDoubleAnimation2.From = 1;
+            myDoubleAnimation2.To = 0;
+            myDoubleAnimation2.Duration = new Duration(TimeSpan.FromSeconds(1.5));
+            myDoubleAnimation2.RepeatBehavior = RepeatBehavior.Forever;
+            blueRectangle.BeginAnimation(Ellipse.WidthProperty, myDoubleAnimation);
+            blueRectangle.BeginAnimation(Ellipse.HeightProperty, myDoubleAnimation);
+            blueRectangle.BeginAnimation(Ellipse.OpacityProperty, myDoubleAnimation2);
+        }
+    }
+}
---
+++ ToggleSwitch/ToggleSwitch/ToggleButton.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToggleSwitch
+{
+    /// <summary>
+    /// Interaction logic for ToggleButton.xaml
+    /// </summary>
+    public partial class ToggleButton : UserControl
+    {
+        Thickness LeftSide = new Thickness(-35, 0, 0, 0);
+        Thickness RightSide = new Thickness(0, 0, -35, 0);
+        private bool Toggled = false;
+
+        public ToggleButton()
+        {
+            InitializeComponent();
+            Toggled = false;
+            Dot.Margin = LeftSide;
+            Forward.Opacity = 1;
+            Reverse.Opacity = 0.6;
+        }
+
+        public bool Toggled1 { get => Toggled; set => Toggled = value; }
+
+        private void Dot_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!Toggled)
+            {
+                Toggled = true;
+                Dot.Margin = RightSide;
+                Forward.Opacity = 0.6;
+                Reverse.Opacity = 1;
+            }
+            else
+            {
+                Toggled = false;
+                Dot.Margin = LeftSide;
+                Forward.Opacity = 1;
+                Reverse.Opacity = 0.6;
+            }
+        }
+
+        private void Back_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!Toggled)
+            {
+                Toggled = true;
+                Dot.Margin = RightSide;
+                Forward.Opacity = 0.6;
+                Reverse.Opacity = 1;
+            }
+            else
+            {
+                Toggled = false;
+                Dot.Margin = LeftSide;
+                Forward.Opacity = 1;
+                Reverse.Opacity = 0.6;
+            }
+        }
+    }
+}
